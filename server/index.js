a80e269a6f43388791c48644978590491535bce2
loic khodarkovsky <lkhodarkovsky@gmail.com>
2024-02-09 15:54:12 +0100

    feat: clean up routes and comments

--- server/index.js
+++ server/index.js
@@ -4,19 +4,12 @@ const app = express();
 const port = 3000;
 const session = require('express-session');
 const SECRET = process.env.SECRET || 'SECRET'
-// const cloudinary = require('cloudinary').v2;
 const router = require('./router');
 
 const corsConfig = {
   origin: 'http://localhost:5173',
   credentials: true,
 };
-
-// cloudinary.config({
-//   cloud_name: 'dyif3hely',
-//   api_key: '',
-//   api_secret: '',
-// });
 
 app.use(cors(corsConfig));
 app.use(express.json());
@@ -37,7 +30,6 @@ app.use(
 );
 
 app.use(router);
-
 
 app.listen(port, () => {
   console.log(`App listening on port ${port} 🐬`);
